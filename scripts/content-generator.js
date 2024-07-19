4ec0ba7f5e2204df3d7f3e7f607c5a4f3b16eb40
matthewT95 <matt.taormina@outlook.com>
2024-07-18 20:22:06 -0400

    fixed url again

--- scripts/content-generator.js
+++ scripts/content-generator.js
@@ -188,7 +188,7 @@ function regenerateContent(mode = "u") {
   else if (generationMode == "image") {
     let {imageHeight,imageWidth}=generationSettings;
     let seed=getRandomIntInclusive(0,2000);
-    HTMLContent="<img src='http:///pseudocontent.matthew-taormina.com/Modules/PlaceHolderImage/?seed="+seed+"&width="+imageWidth+"&height="+imageHeight+"'/>";
+    HTMLContent="<img src='http:///pseudocontent.matthew-taormina.com/modules/place-holder-image/?seed="+seed+"&width="+imageWidth+"&height="+imageHeight+"'/>";
   }
 
   // Inject content
