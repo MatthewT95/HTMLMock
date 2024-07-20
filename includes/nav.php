a504ec769c643aa5361e6b50a0553a56baadf307
matthewT95 <matt.taormina@outlook.com>
2024-07-20 10:50:35 -0400

    Created home page for pseudocontent

---
+++ includes/nav.php
@@ -0,0 +1,21 @@
+<ul id="nav">
+    <?php if($page == 'home')
+    {
+        ?>
+        <li><a href='./index.php'>Home</a></li>
+        <li><a href="./pages/content-generator.html">Content generator</a></li>
+        <!-- <li><a href="./pages/fullscreen-ph.html">Full screen placeholder</a></li> -->
+        <li><a href="./pages/sample1.html">Sample page</a></li>
+        <?php
+    }
+    else
+    {
+        ?>
+        <li><a href='./index.php'>Home</a></li>"
+        <li><a href="./content-generator.html">Content generator</a></li>
+        <!-- <li><a href="./fullscreen-ph.html">Full screen placeholder</a></li> -->
+        <li><a href="./sample1.html">Sample page</a></li>
+        <?php
+    }?>
+
+</ul>
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php $page = 'home'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pseudocontent tools</title>
+    <link rel="stylesheet" href="./styles/all.css">
+    <link rel="stylesheet" href="./styles/nav.css">
+</head>
+<body>
+    <h1>Pseudocontent tools</h1>
+    <?php include './includes/nav.php'; ?>
+    <div id="main">
+    <p>Esiiswm toni bdftn caesrc cstescr rldgtib dt etisoielxo opodwge ealic eluvoa. Pogstmen uivor tig rstirsni wpgabrttm rruyrbn zaig itob ess eerpiet 
+        gnke. Camgrp dufpo ctdnitn bitvm nelr ipahbpy lc yrsss sialimi stk. Nutsl s drdegna otm lsneynt iaeewei nlue ag tr iy claxmi. Oa ogvfi drdegna 
+        ewlso met e acebtir hcrt oyng uaaoro scsc tbr. Aleig recftnm r nns saw ofer ioial eztmi nelr nlue paep. Isats enan knem tabza sdokce eoae wreaitcto sxsget. Rdef bnurnese aeo ircet incer dnmo eegoiimk cstescr. Eei rlai oks esetai plnasi pang asot adeipnb rnrhenue oifl et yacrssp. Lowe naton shooe lioie oimgllm guri cetd itrcs esbap audo istls osaysrna. Mnloc nelr eeyigd teieinar fsmdrg iedi pdylngp eieia imgemt epodru. Irgraen uepahl oduti odi ehtv eerish iniir andr dcems ss enan atasgekw etdo. </p>
+<p>Iousb zyrt sreidrrae ooitu opodwge plnasi nhl lasveri mihni stta. Naierun ag mfiae eha coenlen melbue tbua yratdtimp. Aneu toist esh nnaec nnaec st 
+    sisdsrrrp coenlen. Ot ilh oduti dhs eei ueai ecssf iseecc eueraa sol iniykrss psri nasasoi. Eesii bnfd ohalrw yildasa iy ofoac ncbn nelr ngfcwlr a 
+    tnmlsl ireiwwno. Oduti yacrssp epodru nlue tgmss atonhr ldlt osaysrna edtaauu. Eckhs rn ecan epodru lasveri loomam enrut snm vnomsnhmh ncbn psri. 
+    Sgiihtesitv rruyrbn lsneynt mpzs yilaus st loer apd tjk uncuc oomief. Rdef sgaa pfsc toni iuecrs idl ofer aafhcehn pfsc ls meegy. Ggtnm norebgt ea 
+    rtatbn norebgt ierhmwf bac lugaeup opn gyhnoebe audo a. Dalu eluno cdh toist eegoiimk noa wreaitcto tlnrrt. Hmrg to dm peokl pogstmen toacetnt tti 
+    enu suitler sraba lluhst. </p>
+<p>Claxmi jz abind ealic sie senemprb uaaoro bac r ttytn gnke issga. Teieinar paih lloc epodru teuh rstirsni eoae ntal teieinar tbr cautelo jozl. Ag aeo 
+    tpsoae stl nar beraspri iae tcepk. Ecggrdi ss ac psri stta mearcg netyfg tdcep beraspri uncuc beraspri ewnhirn. Iseecc ei yilaus el tlcbn adb 
+    emigrkw emigrkw. Swrcmlia caxlrl lseya mmeds h wesrm eorensl rllt. Nlue tafirt tiraeeet seo ssn csedi ndea nhrtem iarlf wpgabrttm fi lnd. Ealic 
+    iiu erininel nkosec scsc brchr enlan issga oyng t riupo ilh csoayr. Oyng sdbrxrw mhnime oks ljsn thds pavrrk sbhelore tnpilo nnsqt dedssrrso tntfo 
+    llbs. </p>
+<p>Slspi decalia reakneli iy riupo siupag rn naeen reakneli sivepmic tudpair iseecc. Oa esbap oerts arsaigb lovglh oresi ivaf soco issga skudbe tnmlsl. 
+    Sivepmic vllipc eevo heaa lioie stsg ngeud sivepmic gctde toist ttna clnw. Enudue eckhs sraba apd ea t utpy ee smutprraa giliei. Iyusuumoe oyte tti 
+    anccv soco jtsalehes ncg deerleruh aey mihni irniio eel sgaffe. Njnt ggtnm lgn mwedgpr nea rohu a uncuc sie arqraa adb ienrc tcsrh. Cligipo ilnir 
+    sedmcii tig meegy saltrde om nlue ireiwwno r tlspnaas nsp. Hib odi lm iy lloc srld tiraeeet aey rlnsu nlsrtoresh. Pecyi abroia t dem a lrigfia utetnt 
+    psstnrs ehtv aachwitm sialimi. Eateq meegy suepde apd enlan ivaf nue usoiixt enudue eodsn nsp. Ls lahoi osbigpo eztmi oihf inpint dnmo yacrssp abroia 
+    aornnne. </p>
+<p>Ealic soco iyusuumoe lrares t nslzdflo onen a dnmo. Eiiue zyrt trgr pelps eodsn oeo eddrczh lseya loomam llbs rnrhenue tabza. Oeo toist loomam onnsr 
+    hr tcsrh dufpo t snmafn ls. Lte ar tdaacn ismuer iiu ipglewvnmo oifl qmilu tg odsuh mhnime pavrrk lmcuqgaela. Larmsoe ggtnm rvao iubcmhz mrndqe 
+    diitpial sedpn knem cautelo mca. O iniir eha met akos wfuakc nlue hsiaats esbap tiraeeet teuh akooo. Stl tabza rdef oimgllm bnurnese stl tdica 
+    iyusuumoe ircet iiea ieisoe yrenust. Salguoa nhl tlspnaas agsd andc uirsotprma plnasi qgeani zyrt uaaat ibsctarl. Oomief secksn t cht deerleruh idl 
+    nrd isn as apd bcylo. Aghme iseecc jz t slha aeo sdokce iiu sgaa ness mwedgpr esnto d. Sce nhrtem tf nnraehin aey dem stnocr twaine heaa rtatbn stsiia.
+ </p>
+<p>Uet oks eorensl opn deerleruh anccv eicnpin alwaco nhl raeorv bitvm. Peokl kactmo lowe ost bnfd eddrczh eoae noitit eddrczh. Dem odsuh nctoe ntrrnr 
+    ehtv eirors d bioica vllipc con dnmo. Tabza uet tsao seo recftnm as tlspnaas csoayr if gdsaii dm. Melbue nctoe sbhelore ldrni eceiee hmrg naehgi scsc 
+    nrser. Tpsoae to secn rndpa isn estfrrn vq ecan eamso dem rldgtib ensi. Swrcmlia tssotseo t njnt ttpes trgr rsub nutsl p nslzdflo iseecc eoae. Bnsuzsc 
+    vllipc lowe deuoeei hcrt nudl ialcuii tdeee esetai ssn. Oihf uivor dem shdsspdrl vnomsnhmh esgs seo sertreost nzr sdbrxrw ofoac cligipo. </p>
+<p>Nutsl asot ncg iiu eorensl dcems nasasoi eddrczh. Ahtt uet tg atonhr lk lk nrser atanieipw as. Eceiee nea eseu zorgdn ncokef akete tlspnaas aly gnpud. 
+    A lluhst osbigpo tnohesnnbr jueti onnb alt oerts oslots wpgabrttm nhrtem. Ee ipglewvnmo neicapah sieynoi saltrde idl sclmi snaa erininel hsiaats idd. 
+    Aaeak lisals kactmo t open plrle csedi oerts dufpo sedpn tf ldsg. Gtluhstim or nrep eirors rror li idl lgn tnmlsl. Uettrubin saw edgsoan rtm pdylngp 
+    pfsc xirgef sraba kactmo. Lasveri e gctde oyte wfuakc vq osconrs tf atanieipw. Ile amcegaaso eamso as ishrr eensc rlai rndpa momogla. Dlr iitnin idl 
+    iroemh giliei opn stl ensi tig iiea suepde lugaeup. Snmafn nea pavrrk asruaioa sreidrrae sthn sseabz naehgi. </p>
+<p>Iitnin iy snm ieisoe saw ch teieinar rnrhenue ofoac gu iy wfuakc ynre. Aly mhnime tabza sialimi raeorv ee enan eotf nar tcsrh akooo. 
+    Pecyi eias eerish nnaec aeo irniio fsmdrg oarsnzas dedssrrso. Lbyt eluno inato p ts nrser psstnrs lbyt nqwk tdaacn. Esh ics uaaat aornnne rvao oab 
+    hr ednira eerish. Ttytn ndea seo ile gyhnoebe tdica snm csnelao im hs lnikts audo slrst. Erininel lm hib sedpn lwlehedeid slrst ot mihni iniir sgaa 
+    ldlt. Ea nasasoi dnmo tdaacn twaine ab riupo yildasa tgmss aoeftcre bgewap. Ics idd aasseci eensc eias itrcs iarlf nisito ecssf uirsotprma rhuir 
+    tgmss erininel. Nar irgraen p lc nmtved inpint mst nncenia. </p>
+</div>
+</body>
+</html>
