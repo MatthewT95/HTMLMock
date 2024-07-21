ac8651930ac640bf5d3e333d514c6dd8a7c52b0b
matthewT95 <matt.taormina@outlook.com>
2024-07-21 18:45:10 -0400

    feat: Added documentation page. Added documentation for User interface and normal modes.

---
+++ pages/documentation.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/all.css">
+    <link rel="stylesheet" href="../styles/nav.css">
+    <link rel="stylesheet" href="../styles/multilevel-select.css">
+    <link rel="stylesheet" href="../styles/documentation.css">
+    <title>Documentation</title>
+</head>
+
+<body>
+    <header class="page-header">
+        <h1>Pseudocontent generator documentation</h1>
+        <?php include("../includes/nav.php")?>
+    </header>
+    <div class="ui-documentation">
+        <h2>User Interface</h2>
+        <img src="../images/UserInterface.PNG" alt="Picture of pseudocontent generator's user interface.">
+        <h3>Regenerate button</h3>
+        <p>
+            When this button is clicked the app will generate content according the generation mode
+            and generations settings. The generated content will replace the content in the
+            content area.
+        </p>
+        <h3>Append button</h3>
+        <p>
+            When this button is clicked the app will generate content according the generation mode
+            and generations settings. The generated content will be appended to the content in the
+            content area.
+        </p>
+        <h3>Prepend button</h3>
+        <p>
+            When this button is clicked the app will generate content according the generation mode
+            and generations settings. The generated content will be prepended to the content in the
+            content area.
+        </p>
+        <h3>Web preview/show HTML button</h3>
+        <p>
+            This button which displays text according to what will happen when it is pressed.
+            It allows the user to toggle between seeing the content as it would appear on a
+            web page or seeing the raw HTML.
+        </p>
+        <h3>Toggle Theme button</h3>
+        <p>
+            This button allows the user to toggle the color theme of content area which
+            defaults to dark mode.
+        </p>
+    </div>
+    <div class="generation-modes-doc">
+        <h2>Content generation modes and sub modes</h2>
+        <h3>Normal</h3>
+        <p>
+            In this mode the generator will produce plain content in form of paragraphs
+            and sentences.
+        </p>
+        <h4>Paragraphs</h4>
+        <p>
+            This mode will produce a series of paragraphs. The number of which will be the paragraph count.
+            There will be a random number of sentences between the min and max paragraph length.
+            The sentences will be a random number of words between sentence min and max lengths.
+        </p>
+        <h5>Settings</h5>
+        <ol>
+            <li>Paragraph count</li>
+            <li>Paragraph min length</li>
+            <li>Paragraph max length</li>
+            <li>Sentence min length</li>
+            <li>Sentence max length</li>
+        </ol>
+        <h5>Sample</h5>
+        <div>
+            <p>Iuenetvu maolsnr oi liystzrg rntese kitoee nrzt uatln wfiew. Ososcbitrra iael tmeo ycvee ewycdtem
+                egioadus falncbr ivaoti iditati dgynw c tposji rasralliif. Ietso titbgsi oatniman lrcsd t aknrze onelim
+                gtuws t snicr eis nhits ososcbitrra. Mt ssnk bcng l ilsilae oatniman eplono tlqa nir mdniic ikrsc irett
+                evpubna hameh. Pgcp rst t gercyt ipgaeul ciabrid ogs aseail iyulias ussb iayo. Islnir almsmg cihe reor
+                sidtceo olss cn udto nrs. Hsnfg soiaa sudieou ginopm oeha nccraiig cyye nuins ljrp ieer cyye lsf. Uttbtu
+                ismai inl chlta htsii nmbnk lsf loigli ontied nretu. Urssiaw st ueeee nabpus cepfo oatlers rasiesl chlta
+                rhtesoeo ueeee sudieou ha ccflro. Ngpou t re sussoe iuenetvu dydta ancuodbso elac gngmdg smnesgbh ltr.
+                Asudoo iaysi s tgha sinls asepput ismai oss itnbii menp msgs vue stb elnku. Eoin siin onn diynsaes tst
+                iesrwlir orsias areitiayw. </p>
+            <p>Denh lancnpi uni upavs ipgaeul pruheba irett rpanei strgks adtqemrr soiaa rasralliif aaseolon. Pdksr
+                rhtesoeo onelim tldru cra tposji tlluol aynaiiosde ii meu. Iig efsid nicnesjr gistt pter htrrr ipuea rcr
+                ueeee rte okhreg. Dua wfiew cpls mdsau rnce rhtesoeo loigli ttg. Uni tnemdsi tldru nnueaa rleut lb ttg
+                onn. Ietso tjabussn onn idad nael oolctt warleegt strgks sssfr uni. Mkno ekxn aa mtt syosgr elh rst de
+                ebp. Uaoae nccraiig hncr yior inl oatniman tjabussn eeotoyotn aef srmspium lsf eliigift ghrrf. Dernl ze
+                nir mceiz adtqemrr ulaenot uaoae siin iikcsp thom codx. </p>
+            <p>Edsen ha iikcsp oee eop ohxv epiln ssnk iraaairc ulgnair eeotoyotn. Trlioe obepg eis mdsau codx esaibsloa
+                sdtlcass edtsgpt. Seo rasiesl ayosr aas nltory l criokn aef pruheba asudoo. Wslsns stb oetre e lii
+                falncbr idad efsid nrscs nlst rragtkn sdatki eamnini pvut. Tvn uaaeaa s mdniic irett edsen iagls crfuo
+                upavs nlr ebdeki. A ltr toroo icercr xakgitf kitoee aerghts neuun lidmow pssy. Csa ttg eliigift kaii
+                ncant oiio dua wfiew thom asaoasgsn. Ccflro talsuc nteila olat itoei aiouio eliigift rieece nser sosedr
+                tmeo fdktlgue. Lpie qbngeml codx eetoeo dua dna beeecytb cba eesoe. Gtuws edpii e dgb cienua hameh gt
+                areitiayw ebdeki evair trlioe ylevab tipe. L agptrm fdktlgue dgb silla lt lsn fnruasc nser. Suiashsg
+                nltso nhnasir nlaprrti seaoia nser ibnfiubzitar pter. </p>
+            <p>Lt ssmt aidca yeutpi lancnpi cublbuiti prhnwou nhits. K unrsel dmkss ilrneh ilsilae eonsen rleut rcr nrs
+                pndndo uatln hecdae ccflro tlqa. Cenog diynsaes nlim dacztai rsiimcnat espnv iearli neuun csa aihddm
+                soiaa rkrgc giuhrcsf. Ieer rrl a tlqa menp cmz iraaairc nt teahl. Uil yliw nabpus cmz sinls ueg tipe gt
+                olss trnc iaysi verrsor l. Idad heaoem hhgnra cmz oss hameh pruheba lmimh phnowese hgois rddie. Oatlers
+                tear ltr ipuea cienua mnonprdpao tpoa prhnwou. </p>
+        </div>
+        <h4>Sentences</h4>
+        <p>
+            This mode will produce a series of sentences structure in a single paragraph.
+            There will be as many sentences as specified by sentence count.
+            The sentences will be a random number of words between sentence min and max lengths.
+        </p>
+        <h5>Settings</h5>
+        <ol>
+            <li>Sentence count</li>
+            <li>Sentence min length</li>
+            <li>Sentence max length</li>
+        </ol>
+        <h5>Sample</h5>
+        <div>
+            <p>Ruaspsl hrmdhse aaseolon ipuea plrdenesp asaoasgsn syosgr ibaegc espnv k dshvtaaoi rlt redhrm. Iaysi
+                eiioe uaaeaa tl drurxger sssfr aus ovcd ngst iael ebp hecdae mkno. Nrs rlnrt aewhtu ntm fuar rasiesl aas
+                aosrb csfyeas menp. Rlt aef uclootn stb dydta aas esnss nlr yae ndaf. Tee rragtkn nlst seciir ebp shlu
+                mpclm dsard iuenetvu ltr rlshint. Pvut insoeg lt ulgnair tst agptrm srdonh sdtlcass eafoatu hecdae
+                aynaiiosde uil rte mtt. Nltory dg rasralliif itro rtsumn erbi mpclm ocin ngcse ismai. </p>
+        </div>
+        <h3>List</h3>
+        <p>
+
+        </p>
+        <h3>Table</h3>
+        <p>
+
+        </p>
+        <h3>Header</h3>
+        <p>
+
+        </p>
+        <h3>Image</h3>
+        <p>
+
+        </p>
+    </div>
+</body>
+
+</html>
