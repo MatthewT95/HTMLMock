39c34d427ac4059490753f99ac33325faed8e29b
matthewT95 <matt.taormina@outlook.com>
2024-07-23 05:43:24 -0400

    refactor: Moved list.ul-p mode doc to it own file.

--- pages/documentation.php
+++ pages/documentation.php
@@ -96,69 +96,7 @@
                 <?php include("./doc.list.ol-s.php") ?>
                 <?php include("./doc.list.ul-s.php") ?>
                 <?php include("./doc.list.ol-p.php") ?>
-                <section>
-
-                    <h3 id="header-ul-p">Unordered list of paragraphs</h3>
-                    <button class="btnNavUl-p btnDocNav" data-jump-point="#header-list">Goto top</button>
-                    <p>
-                        This mode produces a unordered list of paragraphs. The paragraphs will be
-                        a random number of sentences from paragraph min to max length. The sentences will be a
-                        random number of words between sentence min and max lengths. It will make as
-                        many items as the user specified in item count setting.
-                    </p>
-                    <h4>Settings</h4>
-                    <ul>
-                        <li>Paragraph min length</li>
-                        <li>Paragraph max length</li>
-                        <li>Sentence min length</li>
-                        <li>Sentence max length</li>
-                        <li>Item count</li>
-                    </ul>
-                    <h4>Sample</h4>
-                    <div>
-                        <ul>
-                            <li>
-                                <p>Ijhs yzua lsg cicrs era tikf doi ynsdhweor tiila cppi edoow. Irgdetam s i ligiaivr
-                                    dnowltct o.
-                                    Ttwti epredeser tl scl otohc ibrohss liaesli xnrcgpta eeihbnie. Iol ta gnea rnsvuo
-                                    tsna
-                                    ieoinpg
-                                    ovsnksn iiycaiar. Zatgu cicrs pfeto rcjr gteldteem uawht knrysce ovsnksn. Mnsagei
-                                    ynsdhweor
-                                    enseiyseote anes tpse eyti eeniue tlascavk wtiay iiupiar. Llu iyg sd ifrlen bcsi
-                                    eanto
-                                    ynsdhweor
-                                    ceonferr guttinda. Auri geg at galriym nosc ecdtla ncdshs ynsdhweor yaoeter oails
-                                    teouse. </p>
-                            </li>
-                            <li>
-                                <p>Yslto otohc mef slio eplrd rnrm n iyg edrtg atnya. Pnt ioec hdsa rsid ieryv tl. Eiut
-                                    ntt
-                                    iy
-                                    rnlshz brlaean tatrni gnnaci. Ldaa lrol n iosers d tlascavk okde. </p>
-                            </li>
-                            <li>
-                                <p>Oebassu s hnofn glsuen rela gnnaci dmemki cpea. Rartol edimts apdaeie eeihbnie
-                                    tlascavk
-                                    sedw
-                                    rdiitstv dimrfha eusu nhazb. Mierk llytsso egesgha rlys rydu aris drkdp oltid rcjr
-                                    ndnriah.
-                                    Tjlumea nnrovld atuttur rtqeg comtabl psesona ijhs ooee bassnun fdlmnc. </p>
-                            </li>
-                            <li>
-                                <p>Aibabysc usere nnc rdpeisoceu bwennu awvasic. Iyg piri tibsse espebi fta lulir e lm
-                                    bassnun taal.
-                                    Mlam lrkyi ntt ieeu ig hoecusutioog. Okde ieaego drmcegvlaa rs odoaasn doi ortrngr
-                                    aeqnct serlse
-                                    spiaes eoatsimv. Edoow bge agflc ae ucrs etco dhss tuse mainh eusu. Tse ieryv ifrlen
-                                    gnurr lms
-                                    epee mlnilp. Yelros smeru secrt tkdrhtr csclneie cehsis teouse mnnsbe aii dyho
-                                    cusii.
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-                </section>
+                <?php include("./doc.list.ul-p.php") ?>
             </section>
             <section class="table-mode-doc">
                 table
