f58ec515c45a448a9a549152b6f30a2837867133
matthewT95 <matt.taormina@outlook.com>
2024-07-23 05:35:09 -0400

    moved normal.sentence mode doc to its own file.

---
+++ pages/doc.normal.sentence.php
@@ -0,0 +1,29 @@
+<section>
+    <h3 id="header-sentence">Sentences</h3>
+    <button class="btnNavUl-p btnDocNav" data-jump-point="#header-normal">Goto top</button>
+    <p>
+        This mode will produce a series of sentences structure in a single paragraph.
+        There will be as many sentences as specified by sentence count.
+        The sentences will be a random number of words between sentence min and max lengths.
+    </p>
+    <h4>Settings</h4>
+    <ul>
+        <li>Sentence count</li>
+        <li>Sentence min length</li>
+        <li>Sentence max length</li>
+    </ul>
+    <h4>Sample</h4>
+    <div>
+        <p>Ruaspsl hrmdhse aaseolon ipuea plrdenesp asaoasgsn syosgr ibaegc espnv k dshvtaaoi rlt
+            redhrm.
+            Iaysi
+            eiioe uaaeaa tl drurxger sssfr aus ovcd ngst iael ebp hecdae mkno. Nrs rlnrt aewhtu ntm fuar
+            rasiesl aas
+            aosrb csfyeas menp. Rlt aef uclootn stb dydta aas esnss nlr yae ndaf. Tee rragtkn nlst
+            seciir
+            ebp shlu
+            mpclm dsard iuenetvu ltr rlshint. Pvut insoeg lt ulgnair tst agptrm srdonh sdtlcass eafoatu
+            hecdae
+            aynaiiosde uil rte mtt. Nltory dg rasralliif itro rtsumn erbi mpclm ocin ngcse ismai. </p>
+    </div>
+</section>
--- pages/documentation.php
+++ pages/documentation.php
@@ -79,35 +79,7 @@
                     <button class="btnNavSentence btnDocNav" data-jump-point="#header-sentence">Goto sentence</button>
                 </div>
                 <?php include("./doc.normal.paragraph.php") ?>
-                <section>
-                    <h3 id="header-sentence">Sentences</h3>
-                    <button class="btnNavUl-p btnDocNav" data-jump-point="#header-normal">Goto top</button>
-                    <p>
-                        This mode will produce a series of sentences structure in a single paragraph.
-                        There will be as many sentences as specified by sentence count.
-                        The sentences will be a random number of words between sentence min and max lengths.
-                    </p>
-                    <h4>Settings</h4>
-                    <ul>
-                        <li>Sentence count</li>
-                        <li>Sentence min length</li>
-                        <li>Sentence max length</li>
-                    </ul>
-                    <h4>Sample</h4>
-                    <div>
-                        <p>Ruaspsl hrmdhse aaseolon ipuea plrdenesp asaoasgsn syosgr ibaegc espnv k dshvtaaoi rlt
-                            redhrm.
-                            Iaysi
-                            eiioe uaaeaa tl drurxger sssfr aus ovcd ngst iael ebp hecdae mkno. Nrs rlnrt aewhtu ntm fuar
-                            rasiesl aas
-                            aosrb csfyeas menp. Rlt aef uclootn stb dydta aas esnss nlr yae ndaf. Tee rragtkn nlst
-                            seciir
-                            ebp shlu
-                            mpclm dsard iuenetvu ltr rlshint. Pvut insoeg lt ulgnair tst agptrm srdonh sdtlcass eafoatu
-                            hecdae
-                            aynaiiosde uil rte mtt. Nltory dg rasralliif itro rtsumn erbi mpclm ocin ngcse ismai. </p>
-                    </div>
-                </section>
+                <?php include("./doc.normal.sentence.php") ?>
             </section>
             <section class="list-mode-doc">
                 <h2 id="header-list">List</h2>
