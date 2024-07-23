347997adfea19ad18337573cec24f2fb1b4428c5
matthewT95 <matt.taormina@outlook.com>
2024-07-23 05:41:29 -0400

    refactor: Move ordered list of paragraphs documentation to separate file

--- pages/documentation.php
+++ pages/documentation.php
@@ -95,62 +95,7 @@
                 </div>
                 <?php include("./doc.list.ol-s.php") ?>
                 <?php include("./doc.list.ul-s.php") ?>
-                <section>
-
-                    <h3 id="header-ol-p">Ordered list of paragraphs</h3>
-                    <button class="btnNavUl-p btnDocNav" data-jump-point="#header-list">Goto top</button>
-                    <p>
-                        This mode produces a ordered list of paragraphs. The paragraphs will be
-                        a random number of sentences from paragraph min to max length. The sentences will be a
-                        random number of words between sentence min and max lengths. It will make as
-                        many items as the user specified in item count setting.
-                    </p>
-                    <h4>Settings</h4>
-                    <ul>
-                        <li>Paragraph min length</li>
-                        <li>Paragraph max length</li>
-                        <li>Sentence min length</li>
-                        <li>Sentence max length</li>
-                        <li>Item count</li>
-                    </ul>
-                    <h4>Sample</h4>
-                    <div>
-                        <ol>
-                            <li>
-                                <p>Nirtns oisnrs soao iol lms oasfadhl. Nss bge orcou dnowltct bac lrkyi ntt. Nosc ecdee
-                                    ra
-                                    reo gnea
-                                    oil n gner. Ectsennc vtisp ieoinpg iaitieu uv htfswi emi a lnsleegc nzcs. </p>
-                            </li>
-                            <li>
-                                <p>Rteernp irgdetam rcmggt a l teoberotl hfsewba. Dttsienisrr arlud sinice ti bassnun
-                                    noaa.
-                                    Naoimsl
-                                    glemip rrtije ridciz fcdwdb ieaego jrsts eotre surr pilri geg. Taneoxfns piiaz t swu
-                                    snov vedep
-                                    nfcl oslroye uliupiib nnpr. </p>
-                            </li>
-                            <li>
-                                <p>Etuo our oshcuna orse aaasdeg mows ae aeqnct. Ortrngr odohii nfcl rins doi featu trcd
-                                    saacu bs
-                                    ldmg cptstna. Uatl dsi ra ive mnpdio ieoinpg pgtsr a notrrr. Llytsso ean teouse
-                                    ncdshs
-                                    utsm
-                                    yebgnis lehrm iyg. Ps aubhen coi gtena ra a nstias. </p>
-                            </li>
-                            <li>
-                                <p>Eanoo sonbnd fusimpuiea srftjo ltitrs peiedo lrlei p. I agflc iyg uliupiib enctl
-                                    vneae
-                                    orrha civr
-                                    lms. Bmmin i ioec edngpe pebud oengeamd rlnzep oo ype leusoopl mhatis. Tpse aaasdeg
-                                    telga a
-                                    ocfniortn eelti iua uietkrca rartol oltid tsercf. Itpf serlse ilaic gnnlhoeh nptc
-                                    edrwl.
-                                    Guttinda til nngcti bac pnin enseiyseote ikssat msslsn hes. </p>
-                            </li>
-                        </ol>
-                    </div>
-                </section>
+                <?php include("./doc.list.ol-p.php") ?>
                 <section>
 
                     <h3 id="header-ul-p">Unordered list of paragraphs</h3>
