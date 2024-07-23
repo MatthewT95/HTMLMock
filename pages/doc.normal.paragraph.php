2116c253e5a84bc0db31d2f15f712c1597a745df
matthewT95 <matt.taormina@outlook.com>
2024-07-23 05:31:04 -0400

    moved normal.paragraph mode documentation to it's own file.

---
+++ pages/doc.normal.paragraph.php
@@ -0,0 +1,87 @@
+<section>
+    <h3 id="header-paragraph">Paragraphs</h3>
+    <button class="btnNavUl-p btnDocNav" data-jump-point="#header-normal">Goto top</button>
+    <p>
+        This mode will produce a series of paragraphs. The number of which will be the paragraph count.
+        There will be a random number of sentences between the min and max paragraph length.
+        The sentences will be a random number of words between sentence min and max lengths.
+    </p>
+    <h4>Settings</h4>
+    <ul>
+        <li>Paragraph count</li>
+        <li>Paragraph min length</li>
+        <li>Paragraph max length</li>
+        <li>Sentence min length</li>
+        <li>Sentence max length</li>
+    </ul>
+    <h4>Sample</h4>
+    <div>
+        <p>Iuenetvu maolsnr oi liystzrg rntese kitoee nrzt uatln wfiew. Ososcbitrra iael tmeo ycvee
+            ewycdtem
+            egioadus falncbr ivaoti iditati dgynw c tposji rasralliif. Ietso titbgsi oatniman lrcsd t
+            aknrze
+            onelim
+            gtuws t snicr eis nhits ososcbitrra. Mt ssnk bcng l ilsilae oatniman eplono tlqa nir mdniic
+            ikrsc irett
+            evpubna hameh. Pgcp rst t gercyt ipgaeul ciabrid ogs aseail iyulias ussb iayo. Islnir almsmg
+            cihe reor
+            sidtceo olss cn udto nrs. Hsnfg soiaa sudieou ginopm oeha nccraiig cyye nuins ljrp ieer cyye
+            lsf. Uttbtu
+            ismai inl chlta htsii nmbnk lsf loigli ontied nretu. Urssiaw st ueeee nabpus cepfo oatlers
+            rasiesl chlta
+            rhtesoeo ueeee sudieou ha ccflro. Ngpou t re sussoe iuenetvu dydta ancuodbso elac gngmdg
+            smnesgbh ltr.
+            Asudoo iaysi s tgha sinls asepput ismai oss itnbii menp msgs vue stb elnku. Eoin siin onn
+            diynsaes tst
+            iesrwlir orsias areitiayw. </p>
+        <p>Denh lancnpi uni upavs ipgaeul pruheba irett rpanei strgks adtqemrr soiaa rasralliif
+            aaseolon.
+            Pdksr
+            rhtesoeo onelim tldru cra tposji tlluol aynaiiosde ii meu. Iig efsid nicnesjr gistt pter
+            htrrr
+            ipuea rcr
+            ueeee rte okhreg. Dua wfiew cpls mdsau rnce rhtesoeo loigli ttg. Uni tnemdsi tldru nnueaa
+            rleut
+            lb ttg
+            onn. Ietso tjabussn onn idad nael oolctt warleegt strgks sssfr uni. Mkno ekxn aa mtt syosgr
+            elh
+            rst de
+            ebp. Uaoae nccraiig hncr yior inl oatniman tjabussn eeotoyotn aef srmspium lsf eliigift
+            ghrrf.
+            Dernl ze
+            nir mceiz adtqemrr ulaenot uaoae siin iikcsp thom codx. </p>
+        <p>Edsen ha iikcsp oee eop ohxv epiln ssnk iraaairc ulgnair eeotoyotn. Trlioe obepg eis mdsau
+            codx
+            esaibsloa
+            sdtlcass edtsgpt. Seo rasiesl ayosr aas nltory l criokn aef pruheba asudoo. Wslsns stb oetre
+            e
+            lii
+            falncbr idad efsid nrscs nlst rragtkn sdatki eamnini pvut. Tvn uaaeaa s mdniic irett edsen
+            iagls
+            crfuo
+            upavs nlr ebdeki. A ltr toroo icercr xakgitf kitoee aerghts neuun lidmow pssy. Csa ttg
+            eliigift
+            kaii
+            ncant oiio dua wfiew thom asaoasgsn. Ccflro talsuc nteila olat itoei aiouio eliigift rieece
+            nser
+            sosedr
+            tmeo fdktlgue. Lpie qbngeml codx eetoeo dua dna beeecytb cba eesoe. Gtuws edpii e dgb cienua
+            hameh gt
+            areitiayw ebdeki evair trlioe ylevab tipe. L agptrm fdktlgue dgb silla lt lsn fnruasc nser.
+            Suiashsg
+            nltso nhnasir nlaprrti seaoia nser ibnfiubzitar pter. </p>
+        <p>Lt ssmt aidca yeutpi lancnpi cublbuiti prhnwou nhits. K unrsel dmkss ilrneh ilsilae eonsen
+            rleut
+            rcr nrs
+            pndndo uatln hecdae ccflro tlqa. Cenog diynsaes nlim dacztai rsiimcnat espnv iearli neuun
+            csa
+            aihddm
+            soiaa rkrgc giuhrcsf. Ieer rrl a tlqa menp cmz iraaairc nt teahl. Uil yliw nabpus cmz sinls
+            ueg
+            tipe gt
+            olss trnc iaysi verrsor l. Idad heaoem hhgnra cmz oss hameh pruheba lmimh phnowese hgois
+            rddie.
+            Oatlers
+            tear ltr ipuea cienua mnonprdpao tpoa prhnwou. </p>
+    </div>
+</section>
