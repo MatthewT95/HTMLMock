e8f89855ac3ce2509d729c001747946e78e2d384
matthewT95 <matt.taormina@outlook.com>
2024-07-23 05:40:05 -0400

    refactor: Move list.ul-s mode doc to separate file.

---
+++ pages/doc.list.ul-s.php
@@ -0,0 +1,30 @@
+<section>
+    <h3 id="header-ul-s">Unordered list of sentences</h3>
+    <button class="btnNavUl-p btnDocNav" data-jump-point="#header-list">Goto top</button>
+    <p>
+        This mode produces a unordered list of sentences. The sentences will be a
+        random number of words between sentence min and max lengths. It will make as
+        many items as the user specified in item count setting.
+    </p>
+    <h4>Settings</h4>
+    <ul>
+        <li>Sentence min length</li>
+        <li>Sentence max length</li>
+        <li>Item count</li>
+    </ul>
+    <h4>Sample</h4>
+    <div>
+        <ul>
+            <li>Noeatd tcioa ectsennc tnsou eetanne ssno stvtaerso anes. </li>
+            <li>Ftreaoe iyg ntt psr psn isrroc ijhs tcioa cpnalm. </li>
+            <li>Alrgarw siorro mbgh ucysiu fta msn. </li>
+            <li>Mnpdio ijhs anei ta cns aeqnct lfitcbrp. </li>
+            <li>Brlaean anf atmietp laa eioc nnpr ise durar pnue gmaacir aplbh. </li>
+            <li>Oasfadhl ncdshs lrlei anf lpocici lm. </li>
+            <li>Hnofn lss phci sc edimts bcsi siorro. </li>
+            <li>Lm pilri tikf ubagta shils esas regvsoee o bstf ei ptinrs. </li>
+            <li>Taal tn bmmin lpocici itotsol creiss screi tse nolfo. </li>
+            <li>Oxalsl tibsse eecix uisml ps anwl lpocici. </li>
+        </ul>
+    </div>
+</section>
