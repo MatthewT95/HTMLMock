5d1a5e3abcf7d75c85efe1bf5eb43a7b11cc4e7d
matthewT95 <matt.taormina@outlook.com>
2024-07-23 21:55:23 -0400

    API can now recursively prase json blue print into html content. Supports innerContent,tags and repetition.

--- APIs/fakeHTMLGenerator.php
+++ APIs/fakeHTMLGenerator.php
@@ -15,18 +15,39 @@ function is_associative($array)
 
 function htmlRecursiveGenerator($blueprint)
 {
+    $tagName="";
+    $repeats = 1;
+    $outputHTML ="";
     // Checks if blueprint is associative
     if (!is_associative($blueprint))
     {
         return "";
     }
+
+    if (array_key_exists("repeats",$blueprint)){
+        if (is_array($blueprint["repeats"]) && is_associative($blueprint["repeats"]))
+        {
+            $minRepeats = $blueprint["repeats"]["min"];
+            $maxRepeats = $blueprint["repeats"]["max"];
+            $repeats=mt_rand($minRepeats,$maxRepeats);
+        }
+        else
+        {
+            $repeats = intval($blueprint["repeats"]);
+        }
+    }
+
+    if (array_key_exists("tagName",$blueprint)){
+        $tagName=$blueprint["tagName"];
+    }
     
+for ($i=0;$i < $repeats;$i++){
     $innerHTML = "";
     if (array_key_exists("innerContent",$blueprint)){
         if (is_array($blueprint["innerContent"]))
         {
             if (is_associative($blueprint["innerContent"])){
-                $innerHTML=htmlRecursiveGenerator($blueprint["innerContent"]);
+                $innerHTML.=htmlRecursiveGenerator($blueprint["innerContent"]);
             }
             else
             {
@@ -38,9 +59,24 @@ function htmlRecursiveGenerator($blueprint)
         }
         else if ($blueprint["innerContent"] == "@paragraph")
         {
-            $innerHTML=gener
+            $innerHTML.=generateFakeParagraph(8,12,8,14,true);
+        }
+        else if ($blueprint["innerContent"] == "@sentence")
+        {
+            $innerHTML.=generateFakeSentence(8,14,true,true,true);
+        }
+        else
+        {
+            $innerHTML="none";
         }
     }
+
+    
+    if ($tagName != "") $outputHTML.="<".$tagName.">";
+    $outputHTML.=$innerHTML;
+    if ($tagName != "") $outputHTML.="</".$tagName.">";
+}
+    return $outputHTML;
 }
 
 $html=htmlRecursiveGenerator($blueprint);
