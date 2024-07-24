5cd4fcef0a41f902ad98faf0e1a12d3024309fbf
matthewT95 <matt.taormina@outlook.com>
2024-07-24 09:37:25 -0400

    updated app names

--- includes/nav.php
+++ includes/nav.php
@@ -3,7 +3,7 @@
     {
         ?>
     <li><a href='./index.php'>Home</a></li>
-    <li><a href="./pages/content-generator.html">Content generator</a></li>
+    <li><a href="./pages/HTMLMock-Basic.html">HTMLMock Basic</a></li>
     <li><a href="./pages/documentation.php">Documentation</a></li>
     <!-- <li><a href="./pages/fullscreen-ph.html">Full screen placeholder</a></li> -->
     <li><a href="./pages/sample1.html">Sample page</a></li>
@@ -14,7 +14,7 @@
     {
         ?>
     <li><a href='../index.php'>Home</a></li>
-    <li><a href="./content-generator.html">Content generator</a></li>
+    <li><a href="./HTMLMock-Basic.html">HTMLMock Basic</a></li>
     <li><a href="./documentation.php">Documentation</a></li>
     <!-- <li><a href="./fullscreen-ph.html">Full screen placeholder</a></li> -->
     <li><a href="./sample1.html">Sample page</a></li>
