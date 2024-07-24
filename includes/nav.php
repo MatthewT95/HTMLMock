af99a4fca1b7b72f10705f1d41b984222453d860
matthewT95 <matt.taormina@outlook.com>
2024-07-24 09:10:51 -0400

    updated nav menu with new page.

--- includes/nav.php
+++ includes/nav.php
@@ -2,22 +2,24 @@
     <?php if($page == 'home')
     {
         ?>
-        <li><a href='./index.php'>Home</a></li>
-        <li><a href="./pages/content-generator.html">Content generator</a></li>
-        <li><a href="./pages/documentation.php">Documentation</a></li>
-        <!-- <li><a href="./pages/fullscreen-ph.html">Full screen placeholder</a></li> -->
-        <li><a href="./pages/sample1.html">Sample page</a></li>
-        <?php
+    <li><a href='./index.php'>Home</a></li>
+    <li><a href="./pages/content-generator.html">Content generator</a></li>
+    <li><a href="./pages/documentation.php">Documentation</a></li>
+    <!-- <li><a href="./pages/fullscreen-ph.html">Full screen placeholder</a></li> -->
+    <li><a href="./pages/sample1.html">Sample page</a></li>
+    <li><a href="./pages/api-FHTMLG-demo.html">API demo FHTMLG</a></li>
+    <?php
     }
     else
     {
         ?>
-        <li><a href='../index.php'>Home</a></li>
-        <li><a href="./content-generator.html">Content generator</a></li>
-        <li><a href="./documentation.php">Documentation</a></li>
-        <!-- <li><a href="./fullscreen-ph.html">Full screen placeholder</a></li> -->
-        <li><a href="./sample1.html">Sample page</a></li>
-        <?php
+    <li><a href='../index.php'>Home</a></li>
+    <li><a href="./content-generator.html">Content generator</a></li>
+    <li><a href="./documentation.php">Documentation</a></li>
+    <!-- <li><a href="./fullscreen-ph.html">Full screen placeholder</a></li> -->
+    <li><a href="./sample1.html">Sample page</a></li>
+    <li><a href="./api-FHTMLG-demo.html">API demo FHTMLG</a></li>
+    <?php
     }?>
 
 </ul>
